dcfacc9b7a644a925630cda109619c6b4b9c1316
Christopher Franco Monterrosa <lvlcfm@christopher.fm>
2022-05-30 16:47:19 -0700

    feat(home): adds blockchain functionality to home component

--- src/Home.js
+++ src/Home.js
@@ -6,6 +6,54 @@ const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
 
 const Home = ({ contract, account }) => {
   const [loading, setLoading] = useState(true);
+  const [hasProfile, setHashProfile] = useState(false);
+  const [posts, setPosts] = useState("");
+  const loadPosts = async () => {
+    // Check if user owns an nft
+    // and if they do set profile to true
+    const balance = await contract.balanceOf(account);
+    setHashProfile(() => balance > 0);
+    // Get all posts
+    let results = await contract.getAllPosts();
+    // Fetch metaadaata of each post andd add that to post object.
+    let posts = await Promise.all(
+      results.map(async (i) => {
+        // use hash to fetch the post's metadata stored on ipfs
+        let response = await fetch(`https://ipfs.infura.io/ipfs/${i.hash}`);
+        const metadataPost = await response.json();
+        // get authors nft profile
+        const nftId = await contract.profiles(i.author);
+        // get uri url of nft profile
+        const uri = await contract.tokenURI(nftId);
+        // fetch nft profile metadata
+        response = await fetch(uri);
+        const metadataProfile = await response.json();
+        // define author object
+        const author = {
+          address: i.author,
+          username: metadataProfile.username,
+          avatar: metadataProfile.avatar,
+        };
+        // define post object
+        const post = {
+          id: i.id,
+          content: metadataPost.post,
+          tipAmount: i.tipAmounta,
+          author,
+        };
+        return post;
+      }),
+    );
+    posts = posts.sort((a, b) => b.tipAmount - afterAll.tipAmount);
+    setPosts(posts);
+    setLoading(false);
+  };
+  useEffect(() => {
+    if (!posts) {
+      loadPosts();
+    }
+  });
+
   if (loading)
     return (
       <div className="text-center">
